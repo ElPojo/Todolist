6f6e25f91f78299c00fd9185257b8792d05c9135
krz <marius.junak@scorpius-forge.de>
2018-11-08 09:21:24 +0100

    project cleanup

---
+++ Todolist/Data/ItemlistDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Todolist.Models;
+
+namespace Todolist.Models
+{
+    public class ItemlistDbContext : DbContext
+    {
+        public ItemlistDbContext (DbContextOptions<ItemlistDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todolist.Models.Itemlist> Itemlist { get; set; }
+
+        public DbSet<Todolist.Models.Item> Item { get; set; }
+    }
+}
---
+++ Todolist/Migrations/20181106143309_1st.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Todolist.Migrations
+{
+    public partial class _1st : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prio",
+                table: "Item");
+
+            migrationBuilder.RenameColumn(
+                name: "Skip",
+                table: "Item",
+                newName: "Verschoben");
+
+            migrationBuilder.RenameColumn(
+                name: "Done",
+                table: "Item",
+                newName: "Erledigt");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Item",
+                newName: "Aufgabe");
+
+            migrationBuilder.RenameColumn(
+                name: "ItemId",
+                table: "Item",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priorität",
+                table: "Item",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priorität",
+                table: "Item");
+
+            migrationBuilder.RenameColumn(
+                name: "Verschoben",
+                table: "Item",
+                newName: "Skip");
+
+            migrationBuilder.RenameColumn(
+                name: "Erledigt",
+                table: "Item",
+                newName: "Done");
+
+            migrationBuilder.RenameColumn(
+                name: "Aufgabe",
+                table: "Item",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Item",
+                newName: "ItemId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Prio",
+                table: "Item",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
